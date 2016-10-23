e1719b551534a9d6901c852000e2e6097e758cf0
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-10-22 21:04:26 -0400

    Scrape all voting records

--- scrape/README.md
+++ scrape/README.md
@@ -5,6 +5,14 @@ These scripts are organized as follows:
 - <b>scrape-votes.js</b> scrapes each individual vote page and saves them as HTML files.
 - <b>parse-votes.js</b> parses the HTML files and saves the data in MongoDB and CSV.
 
+Included in the data directory is the output of scraping and parsing the voting documents taken in
+October 2016.
+- <b>data/votes.csv</b> is a tab separated file with all of the parsed voting record data. This
+includes resolutions adopted with and without vote.
+- <b>data/scraped-voting-pages.zip</b> contains the raw HTML files scraped from unbisnet.un.org.
+If you need to reparse the data it is highly recommended to use these HTML files instead of
+scraping from unbisnet.un.org again, as that is significantly slower.
+
 ### Installation and Usage ###
 You should have [NodeJS](https://nodejs.org/en/) v6.7 or higher as well as [MongoDB](https://www.mongodb.com/download-center) installed.
 To install the NodeJS dependencies, in terminal run:
@@ -20,3 +28,26 @@ node parse-votes.js
 ```
 
 The resulting HTML pages and CSV data will be stored in the 'data' directory.
+
+### Basic Usage ###
+If you only need the raw data, download <b>data/votes.csv</b> from this repository. If you need
+to reparse the HTML pages for the resolution records, unzip <b>data/scraped-voting-pages.zip</b>
+and modify and run <b>parse-votes.js</b> with `node parse-votes.js`.
+
+If you need to scrape the data from the live unbisnet.un.org website, then first run:
+<b>scrape-records.js</b> followed by <b>scrape-votes.js</b>. Note, re-scraping should only be done 
+up to date data is really necessary due to the additional time it takes.
+
+### Data Format ###
+The format of <b>data/votes.csv</b> roughly corresponds to the table displayed on voting record
+pages ([example of page here](http://unbisnet.un.org:8080/ipac20/ipac.jsp?session=1B771818O5341.48875&profile=voting&uri=full=3100023~!476573~!0&ri=1&aspect=power&menu=search&source=~!horizon)).
+Note, because some values are multi-line, some arbitrary delimitters have been chosen.
+- <b>related_document</b> uses the word "and" as a delimitter between multiple values. This is
+consistent with the format of other voting records.
+- Fields with a link have a corresponding "\_link" field. E.g. the <b>agenda_information</b> field
+has an <b>agenda_information_link</b> field with the value of the href attribute of the link.
+- The <b>link_to</b> field only takes the English link if it is available.
+- The text of multi-link values is delimitted with an exclamation point "!" and the links are 
+delimitted with a space.
+If different delimitters or format is desired, modify <b>parse-votes.js</b> and re-parse the
+HTML files.
--- scrape/scrape-records.js
+++ scrape/scrape-records.js
@@ -1,7 +1,21 @@
 // Used to scrape A/RES voting record listings from UNBISNET
 //
 // Resulting record listings are stored in a MongoDB collection. Running on Windows 10 Laptop with
-// core i7 and 8GB of RAM took 93 seconds to scrape listings for 8000 records.
+// core i7 and 8GB of RAM took 6.5 minutes to scrape listings for 19796 records.
+//
+// Implementation Note:
+// Since there is an 8000 record limit on search pages, we need to segment the search queries into
+// result numbers of <= 8000 each.
+//
+// Using the year range filter we segment as follows:
+// [0, 1970)    => 3278 records
+// [1970, 1990) => 6053 records
+// [1990, 2000) => 3961 records
+// [2000, inf)  => 6504 records
+//
+// This sums to 19796, which is two less than the number of results without segmenting. This may be
+// due to two records with malformed date fields. However, there are about 400 duplicate records
+// found using this segmenting. This is filtered out in scrape-votes.js.
 
 const $ = require('cheerio')
 , request = require('request')
@@ -9,22 +23,42 @@ const $ = require('cheerio')
 , MongoClient = require('mongodb').MongoClient
 , mongoUri = 'mongodb://localhost:27017/un_voting'
 , unbisnetUrl = 'http://unbisnet.un.org:8080/ipac20/ipac.jsp?&term=A%2FRES&profile=voting&index=.VM'
-, mongoCollection = 'voting_records'
+, mongoCollection = 'voting_records_segmented'
 , npp = 50 // Number of results per page desired.
+, yearSegments = [
+    { lower: 0, upper: 1970, count: 3278 }, // Exclusive bounds.
+    { lower: 1969, upper: 1990, count: 6053 },
+    { lower: 1989, upper: 2000, count: 3961 },
+    { lower: 2000, upper: 3000, count: 6504 }
+    ]
 , concurrency = 8
-, numPages = Math.ceil(8000 / npp) // 19787 should be real number, but 8000 seems to current limit.
 , fs = require('fs')
 , maxRetry = 10
 ;
 
-let db = null // set on initialization
-, retryCount = {} 
+let db = null // Set on initialization.
+, retryCount = {}
+, uniqueRecordId = 1
+, totalExpecting = yearSegments.reduce((sum, seg) => { return sum + seg.count;}, 0)
+, totalRetrieved = 0
 ;
 
+function addUrls(taskQueue) {
+    yearSegments.forEach((segment) => {
+        let numPages = Math.ceil(segment.count / npp);
+        for (let page = 1; page <= numPages; page++) {
+            url = unbisnetUrl + '&npp=' + npp + '&page=' + page
+                + '&ultype=PD01&uloper=>&ullimit=' + segment.lower
+                + '&ultype=PD01&uloper=<&ullimit=' + segment.upper;
+            taskQueue.push(url)
+        }
+    });
+}
 
-function processPage(pageNum, body, taskCallback) {
+
+function processPage(url, body, taskCallback) {
     let rawResults = $(body).find('center > .tableBackground > tr > form > td > .tableBackground');
-    console.log("Processing page " + pageNum);
+    console.log('Processing page ' + url);
     if (rawResults.length == 0) {
         taskCallback(true);
         return;
@@ -33,8 +67,8 @@ function processPage(pageNum, body, taskCallback) {
     rawResults.each((index, rawElem) => {
         let elem = $(rawElem);
         let keys = elem.find('table > tr > td:nth-child(2) .boldBlackFont1');
-        if (keys.length == 0) console.log('Malformed record: ' + (index+1) + ' on page ' + pageNum);
-        let record = { record_num : npp * (pageNum-1) + index };
+        if (keys.length == 0) console.log('Malformed record: ' + (index+1) + ' on page ' + url);
+        let record = { record_num : uniqueRecordId++ };
         keys.each((index, rawElem) => {
             let elem = $(rawElem);
             let key = elem.text().slice(0, -2); // remove trailing space and colon.
@@ -58,36 +92,38 @@ function processPage(pageNum, body, taskCallback) {
                 anchors.each((index, rawElem) => {
                     if ($(rawElem).text() == 'English') eng = $(rawElem);
                 });
-                if (!eng) console.log((index+1), ' on page ', pageNum, ' no english');
+                if (!eng) console.log((index+1), ' on page ', url, ' no english');
                 else record[key + '_link'] = eng.attr('href');
             }
         });
         parsedResults.push(record);
     });
     db.collection(mongoCollection).insertMany(parsedResults, null, () => { taskCallback(); });
+    totalRetrieved += parsedResults.length;
+    console.log(totalRetrieved + '/' + totalExpecting + ' found');
 }
 
 function scrape(callback) {
     let startTime = Date.now();
-    let retry = (pageNum) => {
-        if (!retryCount[pageNum]) retryCount[pageNum] = 1;
-        if (retryCount[pageNum] < maxRetry) {
-            console.log('Retrying fetch for page ' + pageNum);
-            retryCount[pageNum]++;
-            taskQueue.unshift(pageNum);
+    let retry = (url) => {
+        if (!retryCount[url]) retryCount[url] = 1;
+        if (retryCount[url] < maxRetry) {
+            console.log('Retrying fetch for page ' + url);
+            retryCount[url]++;
+            taskQueue.unshift(url);
         } else {
-            console.error('Max retry hit for page ' + pageNum);
+            console.error('Max retry hit for page ' + url);
         }
     }
-    let taskQueue = async.queue((pageNum, taskCallback) => {
-        request(unbisnetUrl + "&page=" + pageNum + "&npp=" + npp, (err, resp, body) => {
+    let taskQueue = async.queue((url, taskCallback) => {
+        request(url, (err, resp, body) => {
             if (err) {
-                retry(pageNum);
+                retry(url);
                 taskCallback();
                 return;
             }
-            processPage(pageNum, body, (err) => {
-                if (err) retry(pageNum);
+            processPage(url, body, (err) => {
+                if (err) retry(url);
                 taskCallback();
             });
         });
@@ -100,7 +136,7 @@ function scrape(callback) {
     };
 
     // Enqueue them all
-    for (let i = 1; i <= numPages; i++)  taskQueue.push(i);
+    addUrls(taskQueue);
 }
 
 MongoClient.connect(mongoUri, (err, mongoDb) => {
